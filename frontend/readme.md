679bd6de96a9051fd29894273cfbc7ce1a3bf96b
Lucas Belin <lucasbelinpro@gmail.com>
2022-07-02 23:19:14 +0200

    add frontend template

---
+++ frontend/readme.md
@@ -0,0 +1,6 @@
+# # This is a template Vite React Typescript project with the following dependencies :
+    - Tailwindcss
+    - Zod
+    - Axios
+    - React Query
+
---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <h1 className='text-black font-bold text-3xl'>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
