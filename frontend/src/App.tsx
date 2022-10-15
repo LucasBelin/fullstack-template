170152a7b70db31a7fe0065095028a73ca74456e
Lucas Belin <lucasbelinpro@gmail.com>
2022-10-15 23:20:32 +0200

    feat(cors): add cors config in backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+    fetch("http://localhost:8080/api/text/2")
+      .then(res => res.json())
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <h1 className='text-black font-bold text-3xl'>Hello World</h1>
+      <h1 className="text-black font-bold text-3xl">Hello World</h1>
     </div>
   )
 }
