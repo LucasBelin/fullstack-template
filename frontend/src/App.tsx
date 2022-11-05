aaa3269af7669d9d9349fca529e0859bf8250c09
Lucas Belin <lucasbelinpro@gmail.com>
2022-11-05 21:18:37 +0100

    remove react-query remnants

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import { QueryClient, QueryClientProvider } from "react-query"
 import TextComponent from "./components/TextComponent"
 
 function App() {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <div className="h-screen w-screen flex items-center flex-col bg-slate-900 pt-28 gap-28">
-        <h1 className="text-white font-bold text-3xl mb-5">Hello World</h1>
-        <TextComponent />
-      </div>
-    </QueryClientProvider>
+    <div className="h-screen w-screen flex items-center flex-col bg-slate-900 pt-28 gap-28">
+      <h1 className="text-white font-bold text-3xl mb-5">Hello World</h1>
+      <TextComponent />
+    </div>
   )
 }
 
